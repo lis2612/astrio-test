82245fa0be0d5f566d6e6839278d83c825270411
Ilya Lyamin <lyamin.ilya.s@gmail.com>
2022-02-03 17:52:11 +0400

    visual ready

---
+++ src/components/brandsList.vue
@@ -0,0 +1,126 @@
+<template>
+  <q-list>
+    <q-item clickable :active="val === true" @click="selectAll" v-ripple>
+      <q-item-section avatar>
+        <q-checkbox v-model="val" />
+      </q-item-section>
+      <q-item-section> All brands </q-item-section>
+    </q-item>
+    <template v-for="(menuItem, index) in menuList" :key="index">
+      <q-item
+        clickable
+        :active="menuItem.checked === true"
+        @click="invertChecked(menuItem.id)"
+        v-ripple
+      >
+        <q-item-section avatar>
+          <q-checkbox v-model="menuItem.checked" />
+        </q-item-section>
+        <q-item-section>
+          {{ menuItem.title }}
+        </q-item-section>
+      </q-item>
+      <q-separator :key="'sep' + index" v-if="menuItem.separator" />
+    </template>
+  </q-list>
+</template>
+
+<script>
+  import { ref } from "vue";
+  export default {
+    setup() {
+      const val = ref(false);
+      // const leftDrawerOpen = ref(false);
+      const menuList = ref(
+        JSON.parse(`[
+                          {
+                            "id": 1,
+                            "title": "Brand 1",
+                            "sort": "100",
+                            "code": "brand_1"
+                          },
+                          {
+                            "id": 2,
+                            "title": "Brand 2",
+                            "sort": "200",
+                            "code": "brand_2"
+                          },
+                          {
+                            "id": 3,
+                            "title": "Brand 3",
+                            "sort": "300",
+                            "code": "brand_3"
+                          },
+                          {
+                            "id": 4,
+                            "title": "Brand 4",
+                            "sort": "400",
+                            "code": "brand_4"
+                          },
+                          {
+                            "id": 5,
+                            "title": "Brand 5",
+                            "sort": "500",
+                            "code": "brand_5"
+                          },
+                          {
+                            "id": 6,
+                            "title": "Brand 6",
+                            "sort": "600",
+                            "code": "brand_6"
+                          },
+                          {
+                            "id": 7,
+                            "title": "Brand 7",
+                            "sort": "700",
+                            "code": "brand_7"
+                          },
+                          {
+                            "id": 8,
+                            "title": "Brand 8",
+                            "sort": "700",
+                            "code": "brand_8"
+                          },
+                          {
+                            "id": 9,
+                            "title": "Brand 9",
+                            "sort": "900",
+                            "code": "brand_9"
+                          }
+                        ]`)
+      );
+      for (const item of menuList.value) {
+        item.checked = true;
+      }
+
+      return {
+        menuList,
+        val,
+        // leftDrawerOpen,
+        // toggleLeftDrawer() {
+        //   leftDrawerOpen.value = !leftDrawerOpen.value;
+        // },
+        selectAll() {
+          if (val.value === true) {
+            for (const item of menuList.value) {
+              item.checked = false;
+            }
+            val.value = false;
+            console.log(val.value);
+          } else {
+            for (const item of menuList.value) {
+              item.checked = true;
+            }
+            val.value = true;
+          }
+        },
+        invertChecked(id) {
+          for (const item of menuList.value) {
+            if (item.id === id) item.checked = !item.checked;
+          }
+        },
+      };
+    },
+  };
+</script>
+
---
+++ src/main.js
@@ -0,0 +1,44 @@
+import { createApp } from 'vue'
+import { Quasar } from 'quasar'
+import quasarLang from 'quasar/lang/ru'
+
+// Import icon libraries
+import '@quasar/extras/roboto-font/roboto-font.css'
+import '@quasar/extras/material-icons/material-icons.css'
+import '@quasar/extras/material-icons-outlined/material-icons-outlined.css'
+import '@quasar/extras/material-icons-round/material-icons-round.css'
+import '@quasar/extras/material-icons-sharp/material-icons-sharp.css'
+import '@quasar/extras/ionicons-v4/ionicons-v4.css'
+
+// A few examples for animations from Animate.css:
+// import @quasar/extras/animate/fadeIn.css
+// import @quasar/extras/animate/fadeOut.css
+
+// Import Quasar css
+import 'quasar/src/css/index.sass'
+
+// Assumes your root component is App.vue
+// and placed in same folder as main.js
+import App from './App.vue'
+
+const myApp = createApp(App)
+
+myApp.use(Quasar, {
+  plugins: {}, // import Quasar plugins and add here
+  lang: quasarLang,
+  /*
+  config: {
+    brand: {
+      // primary: '#e46262',
+      // ... or all other brand colors
+    },
+    notify: {...}, // default set of options for Notify Quasar plugin
+    loading: {...}, // default set of options for Loading Quasar plugin
+    loadingBar: { ... }, // settings for LoadingBar Quasar plugin
+    // ..and many more (check Installation card on each Quasar component/directive/plugin)
+  }
+  */
+})
+
+// Assumes you have a <div id="app"></div> in your index.html
+myApp.mount('#app')
