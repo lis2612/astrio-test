c4706c45332a87c16f7649f9034ae67b2b06576b
Ilya Lyamin <lyamin.ilya.s@gmail.com>
2022-02-07 13:22:17 +0400

    refactoring brandsList

--- src/components/brandsList.vue
+++ src/components/brandsList.vue
@@ -9,7 +9,6 @@
         clickable
         :active="menuItem.checked === true"
         @click="invertChecked(menuItem.id)"
-        v-ripple
       >
         <q-item-section align="left">
           {{ menuItem.title }}
@@ -29,68 +28,13 @@
 
 <script>
   import { ref } from "vue";
+  import data from "./../data/data";
   export default {
     setup(_, { emit }) {
       const val = ref(true);
       const titleSelectAll = ref("Unselect all");
-      const menuList = ref(
-        JSON.parse(`[
-                                    {
-                                      "id": 1,
-                                      "title": "Brand 1",
-                                      "sort": "100",
-                                      "code": "brand_1"
-                                    },
-                                    {
-                                      "id": 2,
-                                      "title": "Brand 2",
-                                      "sort": "200",
-                                      "code": "brand_2"
-                                    },
-                                    {
-                                      "id": 3,
-                                      "title": "Brand 3",
-                                      "sort": "300",
-                                      "code": "brand_3"
-                                    },
-                                    {
-                                      "id": 4,
-                                      "title": "Brand 4",
-                                      "sort": "400",
-                                      "code": "brand_4"
-                                    },
-                                    {
-                                      "id": 5,
-                                      "title": "Brand 5",
-                                      "sort": "500",
-                                      "code": "brand_5"
-                                    },
-                                    {
-                                      "id": 6,
-                                      "title": "Brand 6",
-                                      "sort": "600",
-                                      "code": "brand_6"
-                                    },
-                                    {
-                                      "id": 7,
-                                      "title": "Brand 7",
-                                      "sort": "700",
-                                      "code": "brand_7"
-                                    },
-                                    {
-                                      "id": 8,
-                                      "title": "Brand 8",
-                                      "sort": "700",
-                                      "code": "brand_8"
-                                    },
-                                    {
-                                      "id": 9,
-                                      "title": "Brand 9",
-                                      "sort": "900",
-                                      "code": "brand_9"
-                                    }
-                                  ]`)
-      );
+      const menuList = ref(data.brandsData);
+
       for (const item of menuList.value) {
         item.checked = true;
       }
@@ -112,7 +56,6 @@
       function invertChecked(id) {
         for (const item of menuList.value) {
           if (item.id === id) item.checked = !item.checked;
-          // titleSelectAll='Select all'
         }
       }
       function applyFilter() {
@@ -123,7 +66,6 @@
           }
         }
         emit("applyFilter", filter);
-        // console.log(filter);
       }
       return {
         menuList,
